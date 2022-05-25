6d6c8b872d9f82e26199fedaaff374b4ce53c7ce
faizalajiw <faizalajiwibowo7@gmail.com>
2022-05-25 21:50:22 +0700

    validating and resetting logic

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -7,10 +7,22 @@ import Button from "../UI/Button";
 const AddUser = (props) => {
   const [username, setUsername] = useState("");
   const [age, setAge] = useState("");
-  
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    // jika ada whitespace, maka tidak akan diinputkan ke dalam database
+    if (username.trim().length === 0 || age.trim().length === 0) {
+      return;
+    }
+
+    if (+username < 1) {
+      return;
+    }
+
     console.log(username, age);
+    // set clear input fields
+    setUsername("");
+    setAge("");
   };
 
   const usernameChangedHandler = (event) => {
@@ -25,9 +37,19 @@ const AddUser = (props) => {
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" onChange={usernameChangedHandler} />
+        <input
+          id="username"
+          type="text"
+          value={username}
+          onChange={usernameChangedHandler}
+        />
         <label htmlFor="age">Umur(Tahun)</label>
-        <input id="age" type="number" onChange={ageChangedHandler} />
+        <input
+          id="age"
+          type="number"
+          value={age}
+          onChange={ageChangedHandler}
+        />
         <Button type="submit">Tambah</Button>
       </form>
     </Card>
